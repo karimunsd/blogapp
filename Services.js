528c7268edc0a1ae9d52ee369fff2a14d7b98ae1
Sayyad Karimun <141710281+karimunsd@users.noreply.github.com>
2024-11-06 21:44:42 +0530

    Update Services.js

--- Services.js
+++ Services.js
@@ -1,9 +1,20 @@
 import './Navbar.css';
-function Services(){
-    return(
-        <div>
-            <h3>This is my Services page</h3>
-        </div>
-    );
+
+function Services() {
+  return (
+    <div>
+      <h3>This is the Services page</h3>
+      <p>Here you can find various services provided by our blog platform:</p>
+      <ul>
+        <li>Write and publish your own blog posts</li>
+        <li>Read blogs from various authors</li>
+        <li>Comment on blog posts and engage with the community</li>
+        <li>Get notifications about new posts and updates</li>
+        <li>Manage your user profile and settings</li>
+      </ul>
+      <p>For more details, please contact us or refer to our documentation.</p>
+    </div>
+  );
 }
+
 export default Services;
