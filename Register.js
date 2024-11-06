b8dadd4c0eb26841973f760b1fb6ee574189b6ef
Sayyad Karimun <141710281+karimunsd@users.noreply.github.com>
2024-11-06 21:40:20 +0530

    Update Register.js

--- Register.js
+++ Register.js
@@ -1,56 +1,75 @@
 import './Style.css';
 
-function Register(){
+function Register() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    return (
-        <div className="wrapper">
-            <div className="form-holder">
-                <h2>Create New Account</h2>
-                <form className="form" onSubmit="http://localhost:5000/register/save" method="post">
-                    <div className="form-group">
-                        <img src="/img/user.png" alt="User Icon" />
-                        <input
-                            type="text"
-                            id="username"
-                            name="username"
-                            placeholder="User name"
-                            
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <img src="/img/email.png" alt="Email Icon" />
-                        <input
-                            type="text"
-                            id="email"
-                            name="email"
-                            placeholder="Email"
-                            
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <img src="/img/lock.png" alt="Lock Icon" />
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder="Password"
-                            
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <button type="submit" >Register</button>
-                    </div>
-                    <div className="form-group">
-                        <a href="/index" className="right-link">
-                            Sign in
-                        </a>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username && email && password) {
+      alert('Registration successful');
+      setUsername('');
+      setEmail('');
+      setPassword('');
+    } else {
+      alert('Please fill out all fields.');
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="form-holder">
+        <h2>Create New Account</h2>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <img src="/img/user.png" alt="User Icon" />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="User name"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <img src="/img/email.png" alt="Email Icon" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <img src="/img/lock.png" alt="Lock Icon" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit">Register</button>
+          </div>
+          <div className="form-group">
+            <a href="/login" className="right-link">
+              Already have an account? Sign in
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
+
 export default Register;
